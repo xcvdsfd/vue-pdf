842989c4b236c8e876a95bafdf492f0d66792f6d
xcvdsfd <xcvdsfd@gmail.com>
2020-08-11 17:03:43 +0800

    pack

--- src/pdfjsWrapper.js
+++ src/pdfjsWrapper.js
@@ -1,6 +1,6 @@
 import {
 	PDFLinkService
-} from 'vue-pdf/pdfjs-dist/lib/web/pdf_link_service';
+} from 'pdfjs-dist/lib/web/pdf_link_service';
 
 var pendingOperation = Promise.resolve();
 export default function (PDFJS) {
